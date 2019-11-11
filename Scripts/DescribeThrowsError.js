596c5c1299cd726b10326cf6e14836a5818f9d9e
Ryan Elfman <Ryan.Elfman@k2.com>
2019-11-11 12:54:27 -0800

    newscripts

---
+++ Scripts/DescribeThrowsError.js
@@ -0,0 +1,9 @@
+metadata = {
+    "systemName": "test1",
+    "displayName": "test2",
+    "description": "test3"
+};
+
+ondescribe = function () {
+    throw new Error("A JS error was thrown.");
+};
---
+++ Scripts/ThrowsError.js
@@ -0,0 +1,1 @@
+throw new Error("A JS error was thrown.");

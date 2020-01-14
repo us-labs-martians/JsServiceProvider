afc3929e27c202ed4e12c2ee62e2606efd7f580a
Ryan Elfman <Ryan.Elfman@k2.com>
2020-01-14 15:24:38 -0800

    Attachments support

--- Scripts/FullDescribe.js
+++ Scripts/FullDescribe.js
@@ -11,7 +11,7 @@ ondescribe = function () {
                 displayName: "OBJECT1",
                 description: "A fake object1",
                 version: "1",
-                isActive: true,
+                isActive: false,
                 properties: {
                     "object1.prop1": {
                         displayName: "OBJECT1.PROP1",
@@ -37,6 +37,17 @@ ondescribe = function () {
                         displayName: "OBJECT1.PROP5",
                         description: "object1.prop5.description",
                         type: "dateTime"
+                    },
+                    "object1.prop6": {
+                      displayName: "OBJECT1.PROP6",
+                      description: "object1.prop6.description",
+                      type: "attachment"
+                    },
+                    "object1.prop7": {
+                      displayName: "OBJECT1.PROP7",
+                      description: "object1.prop7.description",
+                      type: "attachment",
+                      extendedType: "k2.com/2019/image"
                     }
                 },
                 methods: {
@@ -71,35 +82,57 @@ ondescribe = function () {
                                 type: "dateTime"
                             },
                             "object1.parameter6": {
-                                displayName: "OBJECT1.PARAMETER6",
-                                description: "object1.parameter6.description",
-                                type: "string"
+                              displayName: "OBJECT1.PARAMETER6",
+                              description: "object1.parameter6.description",
+                              type: "attachment"
                             },
                             "object1.parameter7": {
-                                displayName: "OBJECT1.PARAMETER7",
-                                description: "object1.parameter7.description",
-                                type: "number"
+                              displayName: "OBJECT1.PARAMETER7",
+                              description: "object1.parameter7.description",
+                              type: "attachment",
+                              extendedType: "k2.com/2019/image"
                             },
                             "object1.parameter8": {
                                 displayName: "OBJECT1.PARAMETER8",
                                 description: "object1.parameter8.description",
-                                type: "decimal"
+                                type: "string"
                             },
                             "object1.parameter9": {
                                 displayName: "OBJECT1.PARAMETER9",
                                 description: "object1.parameter9.description",
-                                type: "boolean"
+                                type: "number"
                             },
                             "object1.parameter10": {
                                 displayName: "OBJECT1.PARAMETER10",
                                 description: "object1.parameter10.description",
+                                type: "decimal"
+                            },
+                            "object1.parameter11": {
+                                displayName: "OBJECT1.PARAMETER11",
+                                description: "object1.parameter11.description",
+                                type: "boolean"
+                            },
+                            "object1.parameter12": {
+                                displayName: "OBJECT1.PARAMETER12",
+                                description: "object1.parameter12.description",
                                 type: "dateTime"
+                            },
+                            "object1.parameter13": {
+                                displayName: "OBJECT1.PARAMETER13",
+                                description: "object1.parameter13.description",
+                                type: "attachment"
+                            },
+                            "object1.parameter14": {
+                                displayName: "OBJECT1.PARAMETER14",
+                                description: "object1.parameter14.description",
+                                type: "attachment",
+                                extendedType: "k2.com/2019/image"
                             }
                         },
-                        requiredParameters: ["object1.parameter6", "object1.parameter7", "object1.parameter8", "object1.parameter9", "object1.parameter10"],
-                        inputs: ["object1.prop1", "object1.prop2", "object1.prop3", "object1.prop4", "object1.prop5"],
-                        requiredInputs: ["object1.prop1", "object1.prop2", "object1.prop3", "object1.prop4", "object1.prop5"],
-                        outputs: ["object1.prop1", "object1.prop2", "object1.prop3", "object1.prop4", "object1.prop5"]
+                    requiredParameters: ["object1.parameter8", "object1.parameter9", "object1.parameter10", "object1.parameter11", "object1.parameter12", "object1.parameter13", "object1.parameter14"],
+                        inputs: ["object1.prop1", "object1.prop2", "object1.prop3", "object1.prop4", "object1.prop5", "object1.prop6", "object1.prop7"],
+                        requiredInputs: ["object1.prop1", "object1.prop2", "object1.prop3", "object1.prop4", "object1.prop5", "object1.prop6", "object1.prop7"],
+                        outputs: ["object1.prop1", "object1.prop2", "object1.prop3", "object1.prop4", "object1.prop5", "object1.prop6", "object1.prop7"]
                     }
                 }
             }

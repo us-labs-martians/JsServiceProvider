73f3c87065d6fb89afd849cae4e69fbe0729fc19
Reji Dasan <reji@k2.com>
2020-02-14 09:40:25 -0800

    adding a defualt Date method to ExtendedTypeDate Testing

--- Scripts/ExtendedTypeDateExecute.js
+++ Scripts/ExtendedTypeDateExecute.js
@@ -17,7 +17,7 @@ ondescribe = function () {
                 },
                 methods: {
                     "ExecuteDateParams": {
-                        displayName: "Execute Params",
+                        displayName: "ExecuteDateParams",
                         description: "Execute Read method with Params",
                         type: "read",
                         parameters: {
@@ -27,7 +27,7 @@ ondescribe = function () {
                         outputs: ["propDate1"]
                     },
                   "ExecuteDateAndDateTimeParams": {
-                      displayName: "Execute Multile Params",
+                      displayName: "ExecuteDateParams",
                       description: "Execute Read method with Multiple Params",
                       type: "read",
                       parameters: {
@@ -38,11 +38,17 @@ ondescribe = function () {
                       outputs: ["propDate1", "propDateTime"]
                     },
                     "ExecuteDateProps": {
-                        displayName: "Execute Props",
+                        displayName: "ExecuteDateProps",
                         description: "Execute Read method with Properties Only",
                         type: "read",
                         inputs: [ "propDate2"],
                         requiredInputs: ["propDate2"],
+                        outputs: ["propDate1"]
+                    },
+                    "ExecuteDateDefault": {
+                        displayName: "ExecuteDateDefault",
+                        description: "Execute Read method with Default date",
+                        type: "read",
                         outputs: ["propDate1"]
                     }
                 }
@@ -71,7 +77,10 @@ function executeTest1(methodName, parameters, properties) {
             break;
         case "ExecuteDateProps":
             executeDateProps(parameters, properties);
-            break;  
+            break;
+        case "ExecuteDateDefault":
+            executeDateDefault();
+            break;
         default: throw new Error("The method " + methodName + " is not supported.");
     }
 }
@@ -82,11 +91,15 @@ function executeDateParams(parameters, properties) {
 }
 
 function executeDateAndDateTimeParams(parameters, properties) {
-  postResult({ "propDate1": parameters["paramDate1"], "propDateTime": parameters["paramDateTime2"]});
+    postResult({ "propDate1": parameters["paramDate1"], "propDateTime": parameters["paramDateTime2"]});
 }
 
 function executeDateProps(parameters, properties) {
     value = properties["propDate2"];
     postResult({ "propDate1": value});
+}
+
+function executeDateDefault() {
+    postResult({ "propDate1": new Date() });
 }
 
